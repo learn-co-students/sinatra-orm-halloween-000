afd75fd12a6f672e4e44f70c7451d93456695932
Artem Eremin <artross.bodhi@gmail.com>
2015-11-15 12:23:31 +0300

    done

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,3 +1,38 @@
 class HousesController < HalloweenController
+  get '/houses' do
+  	erb :'houses/index'
+  end
+
+  get '/houses/new' do
+  	erb :'houses/new'
+  end
+  
+  post '/houses' do
+    @house = House.create(params[:house])
+    redirect "/houses/#{@house.id}"
+  end	
+
+  get '/houses/:id' do |id|
+  	@house = House.find(id)
+  	erb :'houses/show'
+  end
+
+  get '/houses/:id/edit' do |id|
+  	@house = House.find(id)
+  	erb :'houses/edit'
+  end
+  
+  patch '/houses/:id' do |id|
+  	@house = House.find(id)
+    @house.update(params[:house])
+    redirect "/houses/#{@house.id}"
+  end
+
+  patch '/houses/:id/trick-or-treat' do |id|
+  	@house = House.find(id)
+  	@kid = Kid.find(params[:kid_id])
+  	@house.give_candy(@kid)
+    redirect "/houses/#{@house.id}"
+  end    	
 
 end
--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -1,3 +1,27 @@
 class KidsController < HalloweenController
+  get '/kids' do
+  	erb :'kids/index'
+  end
+
+  get '/kids/new' do
+  	erb :'kids/new'
+  end
+
+  post '/kids' do
+  	@kid = Kid.create(params[:kid])
+  	@kid.create_bucket
+  	redirect "/kids/#{@kid.id}"
+  end
+
+  get '/kids/:id' do |id|
+  	@kid = Kid.find(id)
+  	erb :'kids/show'
+  end
+
+  patch '/kids/:id/pig-out' do |id|
+  	@kid = Kid.find(id)
+    @kid.pig_out(params[:consumption].to_i)
+  	redirect "/kids/#{@kid.id}"
+  end
 
 end
